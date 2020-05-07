99455ee7a36b8d473f39a7ec9919124839140fbc
slav dude <muhammadaldorafausta@gmail.com>
2020-05-07 11:32:00 +0700

    tugas polymorphism

---
+++ polymorphism/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_polymorphism
+{
+    public class PrinterWindows
+    {
+        public string Dimension_x { get; set; }
+        public string Dimension_y { get; set; }
+        public string Tipe { get; set; }
+        public virtual void Show(){}
+        public virtual void Print(){}
+    }
+    public class Epson : PrinterWindows
+    {
+        public override void Show()
+        {
+            Dimension_x = "10";
+            Dimension_y = "11";
+            Tipe = "Epson";
+            Console.WriteLine("{0} Display Dimension = {1}*{2}", Tipe, Dimension_x, Dimension_y);
+        }
+        public override void Print()
+        {
+            Console.WriteLine("{0} Printer Printing", Tipe);
+        }
+    }
+
+    public class Canon : PrinterWindows
+    {
+        public override void Show()
+        {
+            Dimension_x = "9.5";
+            Dimension_y = "12";
+            Tipe = "Canon";
+            Console.WriteLine("{0} Display Dimension = {1}*{2}", Tipe, Dimension_x, Dimension_y);
+        }
+        public override void Print()
+        {
+            Console.WriteLine("{0} Printer Printing...", Tipe);
+        }
+    }
+
+    public class LaserJet : PrinterWindows
+    {
+        public override void Show()
+        {
+            Dimension_x = "12";
+            Dimension_y = "12";
+            Tipe = "LaserJet";
+            Console.WriteLine("{0} Display Dimension = {1}*{2}", Tipe, Dimension_x, Dimension_y);
+        }
+        public override void Print()
+        {
+            Console.WriteLine("{0} Printer Printing", Tipe);
+        }
+    }
+}
---
+++ polymorphism/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer;
+            Console.WriteLine("Pilih Printer :\n1. Epson\n2. Canon\n3. LaserJet\n");
+            Console.Write("Nomor Printer [1...3] : ");
+            int jwb = int.Parse(Console.ReadLine());
+
+            if(jwb == 1)
+            {
+                printer = new Epson();
+            }
+            else if(jwb == 2)
+            {
+                printer = new Canon();
+            }
+            else
+            {
+                printer = new LaserJet();
+            }
+
+            printer.Show();
+            printer.Print();
+            Console.ReadKey();
+        }
+    }
+}
