f2c483dfc952cf54befb46d90db4aea664772269
slav dude <muhammadaldorafausta@gmail.com>
2020-06-24 22:02:45 +0700

    tugas ke 8 tentang polymorphism, abstraction, interface, dan collection

---
+++ tugas 8/tugas baru/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace tugas_baru
+{
+    abstract class nilai
+    {
+        public abstract void tampil();
+    }
+
+    interface Ihasil
+    {
+        double hasil();
+    }
+
+    class Nilai : nilai, Ihasil
+    {
+        protected string Id_siswa { set; get; }
+        protected double Nilai_pertama { set; get; }
+        protected double Nilai_kedua { set; get; }
+        protected double Nilai_ketiga { set; get; }
+        public Nilai(string id_siswa, double nilai_pertama,double nilai_kedua,double nilai_ketiga)
+        {
+            Id_siswa = id_siswa;
+            Nilai_pertama = nilai_pertama;
+            Nilai_kedua = nilai_kedua;
+            Nilai_ketiga = nilai_ketiga;
+        }
+
+        public double hasil()
+        {
+            double Hasil = (Nilai_pertama + Nilai_kedua + Nilai_ketiga) / 3;
+            return Hasil;
+        }
+
+        public override void tampil()
+        {
+            Console.WriteLine("id murid \t: {0}", Id_siswa);
+            Console.WriteLine("jumlah nilai\t: {0}", hasil());
+        }
+
+    }
+}
---
+++ tugas 8/tugas baru/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_baru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nilai Murid1 = new Nilai("19.13.2257", 90.2, 72.3, 74.3);
+            Nilai Murid2 = new Nilai("19.12.3321", 99.5, 14.3, 22.4);
+            List < Nilai > list = new List<Nilai>();
+            list.Add(Murid1);
+            list.Add(Murid2);
+
+            Nilai tampil1 = list[0];
+            Nilai tampil2 = list[1];
+            Console.WriteLine("nilai murid 1:");
+            tampil1.tampil();
+            Console.WriteLine();
+            Console.WriteLine("nilai murid 2:");
+            tampil2.tampil();
+            Console.ReadKey();
+        }
+    }
+}
